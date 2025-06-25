cd7b83234f4abde8670060dc53de6579dc22aadc
melissa-24 <mlongenberger@codingdojo.com>
2025-06-25 10:27:14 -0400

    styling for home-cta temp done

--- src/components/Home/CallToAction.jsx
+++ src/components/Home/CallToAction.jsx
@@ -7,10 +7,12 @@ export default function CallToAction() {
         <>
         <div className="cta_wrapper">
             <div className="cta">
-                <h4>Lorem ipsum dolor sit amet</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem, accusamus pariatur magni necessitatibus non, illo porro harum ea nam a aspernatur? Placeat minus dolorem quam accusantium laudantium a ab!</p>
+                <div className="text">
+                    <h4>Lorem ipsum dolor sit amet</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem, accusamus pariatur magni necessitatibus non, illo porro harum ea nam a aspernatur? Placeat minus dolorem quam accusantium laudantium a ab!</p>
+                </div>
                 <div className="buttons">
-                    <button className="contact">Contact Us</button>
+                    <button className="contact contact-gradient">Contact Us</button>
                     <button className="gallery">See Our Work</button>
                 </div>
             </div>
