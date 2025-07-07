9d2c40ba6b5da36da7428473d5300208055183e4
melissa-24 <mlongenberger@codingdojo.com>
2025-07-07 10:58:11 -0400

    styling for home-about

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,13 +1,12 @@
-
+import Jeff from '../../assets/about_img.jpg'
+import './Home.css'
 
 export default function HomeAbout() {
 
     return(
         <>
         <div className="about">
-            <div className="img">
-                <img src="" alt="" />
-            </div>
+            <img className='jeff' src={ Jeff } alt="Jeff" />
             <div className="content">
                 <h3>Jeff & Bobbi Jo</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus quae placeat quas consectetur laudantium possimus voluptates beatae fugiat eum, cumque quam quasi animi voluptatem voluptate quod. Laborum quas nobis voluptatum!</p>
