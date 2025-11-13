05baad9e5a8a2cf9cf87ad63c9f24f07b923e0ca
BrettBeare <beare.logan@yahoo.com>
2025-11-12 21:14:23 -0700

    Implement About image block and text content

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,5 +1,5 @@
 import "./about.css";
-import Jeff from "../../assets/jeff.jpg";
+import Jeff from "../../assets/about.jpg";
 
 export default function AboutUs() {
   return (
@@ -11,8 +11,16 @@ export default function AboutUs() {
         </div>
 
         <div className="about-hero-content">
-          <h2 className="about-hero-title"></h2>
-          <p className="about-hero-text"></p>
+          <h2 className="about-hero-title">Jeff &amp; Bobbi Jo</h2>
+          <p className="about-hero-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            rutrum dui non tincidunt interdum. Cras id vehicula nunc. Morbi
+            placerat, augue a volutpat aliquet, neque massa lacinia odio, eu
+            cursus ligula mauris id ipsum. Nunc vitae ante metus. Etiam vitae
+            nisl et magna feugiat ultricies vitae sit amet velit. Aliquam quis
+            dapibus sapien, ut molestie sem. Nam ut elementum augue. Donec
+            pulvinar ligula a mattis porttitor.
+          </p>
         </div>
       </section>
 
@@ -20,23 +28,23 @@ export default function AboutUs() {
       <section className="about-banner">
         <div className="about-banner-grid">
           <div className="about-banner-item">
-            <p className="about-banner-heading"></p>
-            <p className="about-banner-sub"></p>
+            <p className="about-banner-heading">55 + Years</p>
+            <p className="about-banner-sub">of Experience</p>
           </div>
 
           <div className="about-banner-item">
-            <p className="about-banner-heading"></p>
-            <p className="about-banner-sub"></p>
+            <p className="about-banner-heading">Mobile Welding</p>
+            <p className="about-banner-sub">Services Across NC</p>
           </div>
 
           <div className="about-banner-item">
-            <p className="about-banner-heading"></p>
-            <p className="about-banner-sub"></p>
+            <p className="about-banner-heading">Custom Builds</p>
+            <p className="about-banner-sub">&amp; Custom Metalwork</p>
           </div>
 
           <div className="about-banner-item">
-            <p className="about-banner-heading"></p>
-            <p className="about-banner-sub"></p>
+            <p className="about-banner-heading">Veteran Owned</p>
+            <p className="about-banner-sub">&amp; Family Run</p>
           </div>
         </div>
       </section>
